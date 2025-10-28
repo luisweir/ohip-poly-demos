896ed8f68050d12068f8a6babc102b6c9968d4ed
Luis Weir <luis.weir@oracle.com>
2025-10-28 05:46:11 +0000

    Removed streaming as it's no longer required as poly has native streaming support

--- src/streaming-client/deployFx.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-CONTEXT=ohip.streaming
-npx poly function add sclient src/streaming-client/sclient.ts --context ohip.streaming --server --description "OHIP Streaming API client for PolyAPI"
--- src/streaming-client/runner.ts
+++
@@ -1,8 +0,0 @@
-import poly from 'polyapi';
-
-// uncomment to test locally
-poly.ohip.streaming.sclient()
-  .then(status => console.log(status))
-  .catch(error => console.error('Error in sclient:', error.message));
-  
-
